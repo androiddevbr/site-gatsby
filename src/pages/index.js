3c34b4d5a48e979fb0ce8bb7b4add458f5607015
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2020-02-27 15:08:13 -0300

    update site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,26 @@
 import React from "react";
-import {createMemoryHistory} from "history";
-import {Route, Router, Switch} from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { Route, Router, Switch } from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 import 'typeface-roboto';
 import 'typeface-roboto-slab';
 // pages for this product
 import LandingPage from "./LandingPage/LandingPage.jsx";
+import Helmet from "react-helmet";
 
 let hist = createMemoryHistory();
 
 export default () => (
-  <Router history={hist}>
-    <Switch>
-      <Route path="/" component={LandingPage} />
-    </Switch>
-  </Router>
+  <div className="application">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Android DEV BR</title>
+    </Helmet>
+    <Router history={hist}>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  </div>
 );
