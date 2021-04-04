5e9aac463f288d66e693612ffdb3ddd8dd96b192
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2021-04-03 21:39:46 -0300

    update seo config

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `android-dev-br`,
+        short_name: `adbr`,
         start_url: `/`,
         background_color: `#259347`,
         theme_color: `#259347`,
